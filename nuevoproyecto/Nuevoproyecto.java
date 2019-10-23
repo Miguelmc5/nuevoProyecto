bbd3806d1d6256ea2c0a250db7c0bf5994b8e91c
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-10-23 13:21:35 +0200

    por poner algo

---
+++ nuevoproyecto/Nuevoproyecto.java
@@ -0,0 +1,20 @@
+package nuevoproyecto;
+
+/**
+ *
+ * @author mmartinezcosta
+ */
+public class Nuevoproyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+        System.out.println("esto no sirve para nada");
+    
+    
+    }
+    
+}
